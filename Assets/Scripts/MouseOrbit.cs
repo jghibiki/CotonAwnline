b102f093658afbe77fbdb8c1b36049de83d77c5f
jghibiki <jghibiki.games@gmail.com>
2018-02-07 23:37:09 -0600

    Adds movement

---
+++ Assets/Scripts/MouseOrbit.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseOrbit : MonoBehaviour
+{
+
+    public Transform target;
+    public int degrees = 0;
+    public int dragSpeed = 2;
+	public int depth = -1;
+
+	public float speed = .001f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+		if(target != null){
+
+			transform.position = target.position + new Vector3(0, 0, depth);
+
+			var x = Input.GetAxis("Horizontal");
+			var y = Input.GetAxis("Vertical");
+
+			Vector3 moveVector = (transform.forward * y) + (transform.right * x);
+			moveVector *= speed * Time.deltaTime; 
+			moveVector.y = 0;
+
+			target.transform.Translate(moveVector);
+
+			if(Input.GetAxis("Mouse ScrollWheel") > 0 && GetComponent<Camera>().fieldOfView > 10){
+				GetComponent<Camera>().fieldOfView --;
+			}
+			if(Input.GetAxis("Mouse ScrollWheel") < 0 && GetComponent<Camera>().fieldOfView < 100){
+				GetComponent<Camera>().fieldOfView ++;
+			}
+
+			if (Input.GetMouseButton(1))
+			{
+				degrees = 10;
+				transform.RotateAround(
+					target.position,
+					Vector3.up,
+					Input.GetAxis("Mouse X") * degrees
+				);
+
+				transform.RotateAround(
+					target.position,
+					Vector3.left,
+					Input.GetAxis("Mouse Y") * dragSpeed
+				);
+
+				transform.rotation = Quaternion.Euler(
+					transform.eulerAngles.x,
+					transform.eulerAngles.y,
+					0f	
+				);
+						
+			}
+
+			if (!Input.GetMouseButton(1))
+			{
+				transform.RotateAround(
+					target.position,
+					Vector3.up,
+					degrees * Time.deltaTime
+				);
+			}
+			else
+			{
+				degrees = 0;
+			}
+
+            var newPos = target.transform.position;
+			newPos.y = Mathf.Clamp(newPos.y, 0.0f, 200f);
+			Debug.Log(newPos);
+			target.transform.position = newPos;
+
+		}
+		
+    }
+
+	public void SetTarget(Transform target){
+		this.target = target;
+	}
+}
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,24 +5,69 @@ using UnityEngine.Networking;
 
 public class PlayerMove : NetworkBehaviour {
 
+	public GameObject chipPrefab;
+
+	float sensitivityX = 1f;
+	float sensitivityY = 1f;
+	float minimumY = 1f;
+	float maximumY = 2f;
+
+	float speed = 10f;
+
 	public override void OnStartLocalPlayer(){
 		GetComponent<MeshRenderer>().material.color = Color.red;
+
+		Camera.main.GetComponent<MouseOrbit>().SetTarget(gameObject.transform);
 	}
 
-	// Use this for initialization
-	void Start () {
+	void Start(){
+
+	}
+
+	// Update is called once per frame
+	void Update () {
 
         if (!isLocalPlayer)
             return;
 
-        var x = Input.GetAxis("Horizontal")*0.1f;
-        var z = Input.GetAxis("Vertical")*0.1f;
 
-        transform.Translate(x, 0, z);
-	}
-	
-	// Update is called once per frame
-	void Update () {
+		if(Input.GetKey(KeyCode.R)){
+			
+			Vector3 moveVector = (transform.forward * speed) + (transform.right * speed);
+			moveVector.y += speed * Time.deltaTime; 
+			moveVector.x = 0; 
+			moveVector.z = 0; 
+			transform.Translate(moveVector);
+		}
+
+		if(Input.GetKey(KeyCode.F)){
+			
+			Vector3 moveVector = (transform.forward * speed) + (transform.right * speed);
+			moveVector.y -= speed * Time.deltaTime; 
+			moveVector.x = 0; 
+			moveVector.z = 0; 
+			transform.Translate(moveVector);
+		}
 		
+		if(Input.GetKeyUp(KeyCode.Space)){
+
+			RaycastHit hit;
+
+			Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0)); 
+			 
+			if(Physics.Raycast(ray, out hit, 100f)){
+				CmdCreateChip(hit.point);
+			}
+		}
+
+	}
+
+	[Command]
+	void CmdCreateChip(Vector3 position){
+		var chip = (GameObject)Instantiate(
+			chipPrefab,
+			position - transform.forward,
+			Quaternion.identity
+		);
 	}
 }
