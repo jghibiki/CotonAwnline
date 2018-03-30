67b8b6080d6d9c0ce9e81fc7fe74b7b30b39c007
jghibiki <jghibiki.games@gmail.com>
2018-03-30 15:58:32 -0500

    initial attempt at networked transform syncs from client

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -409,6 +409,12 @@ public class PlayerInteract : NetworkBehaviour {
 					objectBeingDragged.rotation = Quaternion.Euler(rotation.eulerAngles.x+180, rotation.eulerAngles.y, rotation.eulerAngles.z);
 				}
 
+				var sync_transform = objectBeingDragged.GetComponent<SyncTransform>();
+				if(sync_transform != null){
+					CmdSetAuth(objectBeingDragged.GetComponent<NetworkIdentity>().netId, GetComponent<NetworkIdentity>());
+					sync_transform.OnMouseDrag();
+				}
+
 			}
 		}
 
@@ -584,5 +590,25 @@ public class PlayerInteract : NetworkBehaviour {
 
 		NetworkServer.Spawn(obj);
 	}
+
+	[Command]
+     public void CmdSetAuth(NetworkInstanceId objectId, NetworkIdentity player)
+     {
+         var iObject = NetworkServer.FindLocalObject(objectId);
+         var networkIdentity = iObject.GetComponent<NetworkIdentity>();
+         var otherOwner = networkIdentity.clientAuthorityOwner;        
+ 
+         if (otherOwner == player.connectionToClient)
+         {
+             return;
+         }else
+         {
+             if (otherOwner != null)
+             {
+                 networkIdentity.RemoveClientAuthority(otherOwner);
+             }
+             networkIdentity.AssignClientAuthority(player.connectionToClient);
+         }        
+     }
 
 }
---
+++ Assets/Scripts/SyncTransform.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SyncTransform : NetworkBehaviour {
+
+	Vector3 position1;
+    Quaternion rotation1;
+    bool move;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		move = false;
+	}
+
+	public void OnMouseDrag () {
+		position1 = gameObject.transform.position;
+		rotation1 = gameObject.transform.rotation;
+		CmdSendMovement(position1, rotation1);
+		Debug.Log("On Mouse Drag: ");
+	}
+ 
+	public void OnMouseExit () {
+	
+		Debug.Log("On Mouse Exit: ");
+	}
+	
+	[Command]
+	public void CmdSendMovement(Vector3 position1, Quaternion rotation1)
+	{
+		gameObject.transform.position = position1;
+		gameObject.transform.rotation = rotation1;
+	}
+
+}
